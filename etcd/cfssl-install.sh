ca657ca4c0e038f7e1d1e3f02247352497eb512b
Wanichun Sinpitak <wanichnun.sinpitak@thoughtworks.com>
2020-09-23 12:15:17 +0700

    Add install cfssl script

---
+++ etcd/cfssl-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -f /tmp/cfssl* && rm -rf /tmp/certs && mkdir -p /tmp/certs
+
+curl -L https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 -o /tmp/cfssl
+chmod +x /tmp/cfssl
+sudo mv /tmp/cfssl /usr/local/bin/cfssl
+
+curl -L https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 -o /tmp/cfssljson
+chmod +x /tmp/cfssljson
+sudo mv /tmp/cfssljson /usr/local/bin/cfssljson
+
+/usr/local/bin/cfssl version
+/usr/local/bin/cfssljson -h
